37754fbaf099c06181713dcc4f9916f001736529
liuxi <jz980425@163.com>
2023-02-03 09:43:31 +0800

    1

--- testcase/test_yf.py
+++ testcase/test_yf.py
@@ -13,6 +13,51 @@ jine = random.randint(99, 399)
 # 定义修改金额
 xgjine = random.randint(99, 399)
 
+# 开票
+# 定义开票id
+b = {'id': None}
+c = b['id']
+# 定义8位随机开票号码
+kphm = random.randint(99, 99999999)
+# 定义8位随机开票号码
+xgkphm = random.randint(99, 99999999)
+# 定义开票未含税金额
+hs = jine * 0.85
+# 定义开票含税金额
+whs = jine * 0.15
+
+
+def test_fp():
+    global c
+    # 发票URL
+    fp_url = 'http://192.168.0.217:9901/PurchaseApInvoice'
+    # 发票请求头
+    fp_header = {"Content-Type": "application/json;charset=UTF-8", "authorization": f"Bearer {a}"}
+    # 发票请求体
+    fp_data = {"invoice_list": [
+        {"invoice_total_amount": jine, "invoice_no": kphm, "invoice_time": "2023-02-01T16:00:00.000Z",
+         "invoice_type": 86, "invoice_type_name": "增值税专用发票-HZ(13%)",
+         "invoice_info": [{"invoice_amount": whs, "invoice_tax_amount": hs, "invoice_tax_rate": whs}]}],
+        "remark": "测试开票", "order_list": [
+            {"customer_name": "浙江天健远见科技有限公司", "purchase_order_code": "87654321", "invoice_amount": jine,
+             "not_invoice_amount": "9996611.00"}]}
+    # 发票请求
+    r = requests.post(url=fp_url, json=fp_data, headers=fp_header)
+    # 输出日志
+    logger.logger.debug(f'发送请求:{r}')
+    # 获取发票订单id更新到列表
+    b['id'] = r.json()['data']['id']
+    c = r.json()['data']['id']
+    # 打印json返回数据
+    # print(r.json())
+    # 设置发票成功断言
+    assert r.json()['message'] == 'success'
+    print(r.json())
+
+
+# test_fp()
+
+
 # 折扣
 # 定义折扣id
 zhekou = {'id': None}
